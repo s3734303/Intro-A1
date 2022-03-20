1304bfd6c2d34f97322f15d8e742fef841130786
3734303 <s3734303@student.rmit.edu.au>
2022-03-21 03:30:22 +1100

    scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "home#index"
   get '/' => 'home#index'
   get '/job' => 'job#index'
-  get 'profile' => 'profile#index'
-  get 'project' => 'project#index'
+  get '/profile' => 'profile#index'
+  get '/project' => 'project#index'
 end
